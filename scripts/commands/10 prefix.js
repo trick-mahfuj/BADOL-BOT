39e19a77ca96e93bc033c74df5dd886ec2299bd2
Badolbot2 <Badolfb+3@yandex.com>
2024-10-03 19:18:01 +0600

    Add files via upload

---
+++ scripts/commands/10 prefix.js
@@ -0,0 +1,59 @@
+const dipto = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+const moment = require('moment-timezone');
+const pathFile = __dirname + '/cache/d1pt0.txt';
+if (!fs.existsSync(pathFile))
+  fs.writeFileSync(pathFile, 'true');
+  const isEnable = fs.readFileSync(pathFile, 'utf-8');
+module.exports.config = {
+  name: "prefix",
+  version: "1.0.0",
+  permission: 2,
+  credits: "★𝐌𝟗𝐇𝟒𝐌𝐌𝟒𝐃-𝐁𝟒𝐃𝟗𝐋★",
+  prefix:true,
+  description: "when send ,prefix then response",
+  category: "bot prefix",
+  usages: "prefix",
+  cooldowns: 5,
+};
+module.exports.handleEvent = async ({ api, event }) => {
+  if (isEnable == "true"){
+  const dipto2 = event.body ? event.body.toLowerCase() : '';
+    const GP = "🤍✨𝐑𝐎𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗✨🤍";
+     let d1PInfo = await api.getThreadInfo(event.threadID);
+  let diptoName = d1PInfo.threadName;
+    var time = moment.tz("Asia/Dhaka").format("LLLL");
+  const text = `╭•┄┅═══❁🌺❁═══┅┄•╮\n${GP}\n╰•┄┅═══❁🌺❁═══┅┄•╯\n\n𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 : ${global.config.BOTNAME}\n𝐑𝐎𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 : ｢ ${global.config.PREFIX} ｣\n𝐑𝐎𝐁𝐎𝐓 𝐂𝐌𝐃: ｢ ${client.commands.size} ｣\n𝐓𝐈𝐌𝐄 : ${time}\n𝐆𝐑𝐎𝐔𝐏 𝐍𝐀𝐌𝐄: ${diptoName}\n`
+  //const text2 = text[Math.floor(Math.random() * text.length)];
+const imgur = ["https://i.imgur.com/Yi0QXnn.jpeg"]
+  const link = imgur[Math.floor(Math.random() * imgur.length)];
+  const res = await dipto.get(link, { responseType: 'arraybuffer' })
+const ex = path.extname(link);
+  const filename = __dirname + `/cache/dipto3${ex}`;
+  fs.writeFileSync(filename, Buffer.from(res.data, 'binary'));
+  if (dipto2.indexOf("prefix") ===0|| dipto2.indexOf("Prefix") ===0 )
+  {
+api.sendMessage({body:`${text}`,attachment: fs.createReadStream(filename)},event.threadID,() => fs.unlinkSync(filename),event.messageID)
+  }
+ }
+}
+module.exports.run = async ({api,args, event}) => {
+try {
+  if (args[0] == 'on') {
+    fs.writeFileSync(pathFile, 'true');
+    api.sendMessage('no prefix on successfully', event.threadID, event.messageID);
+  }
+  else if (args[0] == 'off') {
+    fs.writeFileSync(pathFile, 'false');
+    api.sendMessage('no prefix off successfully', event.threadID, event.messageID);
+  }
+  else if (!args[0]){
+    api.sendMessage(`Wrong format ${this.config.name}use off/on`, event.threadID, event.messageID);
+  }
+  }
+  catch(e) {
+    console.log(e);
+  }
+
+}
---
+++ scripts/commands/22 info.js
@@ -0,0 +1,62 @@
+module.exports.config = {
+  name: "info",
+  version: "1.0.1", 
+  permssion: 0,
+  credits: "★𝐌𝟗𝐇𝟒𝐌𝐌𝟒𝐃-𝐁𝟒𝐃𝟗𝐋★",
+  prefix:true,
+  description: "Admin and Bot info.",
+  category: "...",
+  cooldowns: 1,
+  dependencies: 
+  {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+    hours = Math.floor(time / (60 * 60)),
+    minutes = Math.floor((time % (60 * 60)) / 60),
+    seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【hh:mm:ss】");
+var link = ["https://i.imgur.com/Yi0QXnn.jpeg", 
+
+            "https://i.imgur.com/Yi0QXnn.jpeg", 
+
+            "https://i.imgur.com/Yi0QXnn.jpeg",
+
+            "https://i.imgur.com/Yi0QXnn.jpeg"];
+
+var callback = () => api.sendMessage({body:`•—»✨𝐀𝐝𝐦𝐢𝐧 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺
+ •┄┅════❁🌺❁════┅┄•
+
+𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : 𝐁𝟒𝐃𝟗𝐋-𝐁𝟗𝐓___//𝟎𝟎𝟕
+
+𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : 𝐀d𝐦𝐢𝐧 𝐜𝐨𝐧𝐭𝐫𝐨𝐥 𝐓𝐡𝐢𝐬 𝐑𝐨𝐛𝐨𝐭
+
+•┄┅══❁CONCATET❁══┅┄• 
+
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐏𝐚𝐠𝐞 : https://m.me/MBC.K1NG.007
+
+•┄┅═══❁🌺❁═══┅┄•\n🌺✨𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧✨🌺\n •┄┅═══❁🌺❁═══┅┄•
+
+TYPE /help
+
+𝐁𝐨𝐭 𝐍𝐚𝐦𝐞 : ${global.config.BOTNAME}
+
+𝐁𝐨𝐭 𝐏𝐫𝐞𝐟𝐢𝐱 : ${global.config.PREFIX}
+
+•—»✨ 𝐔𝐩𝐭𝐢𝐦𝐞
+
+𝐓𝐨𝐝𝐚𝐲 𝐈𝐬 𝐓𝐢𝐦𝐞 : ${juswa} 
+
+𝐁𝐨𝐭 𝐈𝐬 𝐑𝐮𝐧𝐧𝐢𝐧𝐠 ${hours}:${minutes}:${seconds}.
+
+𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠  ༄🌺\n｢🕋｣${global.config.BOTNAME}｢🕋｣`,attachment: fs.createReadStream(__dirname + "/cache/cyber.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/cyber.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/cyber.jpg")).on("close",() => callback());
+   };
